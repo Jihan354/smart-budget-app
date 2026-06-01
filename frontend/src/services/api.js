6d6e7ae9ede9791222b261cf43c04dbdbbc20ffd
Jihan <muhammadjihan32@gmail.com>
2026-06-02 04:26:43 +0700

    connect frontend to railway

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 // ================= BASE URL =================
 // alamat backend Flask
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://smart-budget-app-production-1fba.up.railway.app";
 
 // ================= EXPENSE =================
 
